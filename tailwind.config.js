9d91413b8d04f0ca21d4918d84ffb7f346eba91d
RupaliD <rupali.dinde@inflectionzone.com>
2024-10-19 10:27:10 +0530

    Added daisyui plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui"
+
 export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    daisyui
+  ],
 }
 
