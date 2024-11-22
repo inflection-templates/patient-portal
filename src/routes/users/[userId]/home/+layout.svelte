c2593dff13d98dd389f5c183ff79ad64f01d6fbe
RupaliD <rupali.dinde@inflectionzone.com>
2024-11-22 12:00:15 +0530

    Refactr code for sidebar

--- src/routes/users/[userId]/home/+layout.svelte
+++ src/routes/users/[userId]/home/+layout.svelte
@@ -1,12 +1,18 @@
 <script>
-	import Navbar from '$lib/components/home/Navbar.svelte';
-	import Sidebar from '$lib/components/home/Sidebar.svelte';
+	import Navbar from '$lib/components/home/navbar.svelte';
+	import Sidebar from '$lib/components/home/sidebar.svelte';
+	import { page } from '$app/stores';
+
+	//////////////////////////////////////////////////////////////////////
+
+	const userId = $page.params.userId;
+
 </script>
 
 <Navbar />
 
-<div class="flex">
-	<Sidebar />
+<div  class="flex">
+	<Sidebar userId={userId} />
 	<div class="flex-1 p-4">
 		<slot />
 	</div>
