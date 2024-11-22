77d70be655e850bbdccf109be98136749d3a04f7
mayur-bankar-inflection <mayur.s.bankar@inflectionzone.com>
2024-11-22 13:47:13 +0530

    responsiveness and fonts

--- src/lib/components/home/vitals.graph.svelte
+++ src/lib/components/home/vitals.graph.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	import BarChart from '../vitals.graphs/Bar.Chart.svelte';
-	import LineChart from '../vitals.graphs/Line.Chart.svelte';
-	import JoinLineChart from '../vitals.graphs/Join.Line.Chart.svelte';
+	import BarChart from './BarChart.svelte';
+	import LineChart from './LineChart.svelte';
+	import JoinLineChart from './JoinLineChart.svelte';
 	export let data;
 	export let title;
 
@@ -10,9 +10,6 @@
 	let value1: any[] = [];
 
 	$: if (data && Array.isArray(data)) {
-		data.sort((a, b) => {
-    return new Date(a.date).getTime() - new Date(b.date).getTime();
-  });
 		date = data.map((item) => item.date);
 		value = data.map((item) => item.value);
 		value1 = data.map((item) => item.value1);
