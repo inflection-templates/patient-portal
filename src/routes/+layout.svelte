eeb113f15b939f205ff6e4de22fad479f6e09d2a
RupaliD <rupali.dinde@inflectionzone.com>
2024-11-22 12:57:04 +0530

    layout.server changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,28 +1,11 @@
-<!-- <script lang="ts">
-  import "../app.css";
-  import { getFlash } from 'sveltekit-flash-message';
-  import { page } from '$app/stores';
-	import { initFlash } from 'sveltekit-flash-message/client';
-
-	const flash = initFlash(page);
-
-</script>
-
-  {#if $flash}
-  {@const bg = $flash.type == 'success' ? '#3D9970' : '#FF4136'}
-  <div style:background-color={bg} class="flash">{$flash.message}</div>
-{/if}
-
-<slot/> -->
-
 <script lang="ts">
-  import "../app.css";
+	import '../app.css';
 	import { page } from '$app/stores';
 	import { addToast } from '$lib/components/toast/toast.store';
 	import Toasts from '$lib/components/toast/toasts.svelte';
 	import { initFlash } from 'sveltekit-flash-message/client';
 
-  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	const flash = initFlash(page);
 
@@ -32,7 +15,7 @@
 			type: $flash.type || 'info',
 			message: $flash.message,
 			dismissible: true,
-      timeout: 9000,
+			timeout: 9000
 		});
 		flash.set(undefined);
 	});
@@ -40,8 +23,3 @@
 
 <Toasts />
 <slot />
-
-
-
-
-
