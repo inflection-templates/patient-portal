9128f2d43f947124a853ddf97bee2928644c8dce
RupaliD <rupali.dinde@inflectionzone.com>
2024-12-19 13:13:59 +0530

    Profile update message fixes

--- src/routes/users/[userId]/my-profile/+page.server.ts
+++ src/routes/users/[userId]/my-profile/+page.server.ts
@@ -3,6 +3,8 @@ import { error, type RequestEvent } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import { z } from 'zod';
 import { zfd } from 'zod-form-data';
+import { redirect } from 'sveltekit-flash-message/server';
+import { errorMessage, successMessage } from '$lib/utils.ts/message.utils';
 
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -102,5 +104,16 @@ export const actions = {
 			result.postalCode,
 			result.imageResourceId
 		);
+
+		if (response.Status == 'failure' || response.HttpCode !== 200) {
+			throw redirect(`/users/${userId}/my-profile`, errorMessage(response.Message), event);
+		}
+		
+		throw redirect(
+            303,
+            `/users/${userId}/my-profile`,
+            successMessage(`Profile updated successfully!`),
+            event
+        );
 	}
 };
